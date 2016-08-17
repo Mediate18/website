34709a1063f38483b130c38b0b0494db905698c9
matjemeisje <matje@taalmonsters.nl>
2016-08-17 11:47:34 +0200

    Add Helwi's info, don't show images on small screens and update headers

--- php/pages/contact.php
+++ php/pages/contact.php
@@ -1,7 +1,7 @@
 <div class="col-xs-12 display-lg">
 	<img src="img/team.jpg" />
 	<span class="banner visible-sm visible-md visible-lg">
-		<h1>Meet our team</h1>
+		<h1>Contact</h1>
 	</span>
 </div>
 <div class="col-xs-12 page-content team">
@@ -24,27 +24,27 @@
 			</div>
 		</div>
 	</div>
-	<h1 class="visible-xs">Meet our team</h1>
+	<h2>Meet our team</h2>
 	<div class="panel panel-default">
 		<div class="panel-body">
-			<img src="img/team/alicia3.jpg" />
+			<img class="visible-sm visible-md visible-lg" src="img/team/alicia3.jpg" />
 			<div>
 				<h2>Prof. dr. Alicia C. Montoya</h2>
 				<p>Alicia Montoya is Principal Investigator of MEDIATE and Professor of French Literature and Culture at Radboud University, The Netherlands. 
-					She is the author of Medievalist Enlightenment: From Charles Perrault to Jean-Jacques Rousseau (Cambridge 2013), Marie-Anne Barbier et la 
-					trag&eacute;die post-classique (Paris 2007, winner of the Keetje Hodson Prize of the Royal Dutch Society for Arts and Sciences) and the co-editor 
-					of several volumes, including  Lumi&egrave;res et histoire / Enlightenment and History (Paris 2010). </p>
+					She is the author of <i>Medievalist Enlightenment: From Charles Perrault to Jean-Jacques Rousseau</i> (Cambridge 2013), <i>Marie-Anne Barbier et la 
+					trag&eacute;die post-classique</i> (Paris 2007, winner of the Keetje Hodson Prize of the Royal Dutch Society for Arts and Sciences) and the co-editor 
+					of several volumes, including  <i>Lumi&egrave;res et histoire / Enlightenment and History</i> (Paris 2010). </p>
 				<p>Email: <a href="mailto:a.montoya@let.ru.nl">a.montoya@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
 	<div class="panel panel-default">
 		<div class="panel-body">
-			<img src="img/team/matje1.png" />
+			<img class="visible-sm visible-md visible-lg" src="img/team/matje1.png" />
 			<div>
 				<h2>Dr. Matje van de Camp</h2>
 				<p>Matje van de Camp is the Postdoctoral Researcher in charge of the creation and disclosure of the MEDIATE database. She obtained a PhD from Tilburg University
-					in March 2016 for her thesis A Link to the Past: Constructing Historical Social Networks from Unstructured Data, and is the winner of the 2012 Leipzig eHumanties 
+					in March 2016 for her thesis <i>A Link to the Past: Constructing Historical Social Networks from Unstructured Data</i>, and is the winner of the 2012 Leipzig eHumanties 
 					Innovation Award. Matje divides her time between MEDIATE and <a href="http://www.taalmonsters.nl" target="_blank">De Taalmonsters</a>, where she works as a consultant and 
 					developer of Natural Language Processing applications.</p>
 				<p>Email: <a href="mailto:m.vandecamp@let.ru.nl">m.vandecamp@let.ru.nl</a></p>
@@ -53,9 +53,14 @@
 	</div>
 	<div class="panel panel-default">
 		<div class="panel-body">
-			<img src="img/team/helwi.jpg" />
+			<img class="visible-sm visible-md visible-lg" src="img/team/helwi.jpg" />
 			<div>
 				<h2>Dr. Helwi Blom</h2>
+				<p>Helwi Blom is involved in MEDIATE as a postdoctoral researcher concerned with mapping the literary field and the place of middlebrow texts within the larger 
+					field, as it emerges from the database findings. She earned her PhD in French Studies from Utrecht University. Her thesis <i>'Vieux Romans' et 'Grand Si&egrave;cle'; 
+					&eacute;ditions et r&eacute;ceptions de la litt&eacute;rature chevaleresque m&eacute;di&eacute;vale dans la France du dix-septi&egrave;me si&egrave;cle</i> focuses on questions regarding the production, 
+					distribution and consumption of books in early modern France. Helwi is also participant in the <a href="http://edpop.wp.hum.uu.nl/" target="_blank">EDPOP</a> (The European Dimensions of Popular print) project.
+				</p>
 				<p>Email: <a href="mailto:h.m.w.c.blom@uu.nl">h.m.w.c.blom@uu.nl</a></p>
 			</div>
 		</div>
