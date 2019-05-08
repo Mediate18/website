17f11ea699f1c7487ae710c8c4cbc2854e17b54b
Micha Hulsbosch <michahulsbosch@gmail.com>
2019-05-08 16:01:27 +0200

    Short hand tags did not work because PHP upgrade. Fixed.

--- php/layout/page.php
+++ php/layout/page.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
-	<? include("php/layout/head.php") ?>
+	<?php include("php/layout/head.php") ?>
 	<body>
-		<? include("php/layout/navbar.php") ?>
+		<?php include("php/layout/navbar.php") ?>
 		<div class="container-fluid ds">
 			<div class="row">
 				<div class="col-xs-10 col-xs-offset-1">
@@ -18,10 +18,10 @@
 				<div class="col-xs-10 col-xs-offset-1">
 					<div class="col-xs-10 col-xs-offset-1 col-lg-8 col-lg-offset-2">
 						<div id="content">
-							<? include("php/layout/tabs.php") ?>
+							<?php include("php/layout/tabs.php") ?>
 							<div id="my-tab-content" class="tab-content">
 								<div class="tab-pane active">
-									<? include("php/pages/".$page.".php") ?>
+									<?php include("php/pages/".$page.".php") ?>
 								</div>
 							</div>
 						</div>
@@ -29,6 +29,6 @@
 				</div>
 			</div>
 		</div>
-		<? include("php/layout/footer.php") ?>
+		<?php include("php/layout/footer.php") ?>
 	</body>
 </html>
--- php/layout/tabs.php
+++ php/layout/tabs.php
@@ -1,23 +1,23 @@
 <ul id="tabs" class="nav nav-justified">
-	<li class="<? echo ($page === 'home' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'home' ? 'active' : '') ?>">
 		<a href="?page=home">HOME</a>
 	</li>
-	<li class="<? echo ($page === 'about' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'about' ? 'active' : '') ?>">
 		<a href="?page=about">ABOUT</a>
 	</li>
-	<li class="<? echo ($page === 'news' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'news' ? 'active' : '') ?>">
 		<a href="?page=news">NEWS</a>
 	</li>
 	<li class="">
 		<a href="http://bibliomediator.nl/">BLOG</a>
 	</li>
-	<li class="<? echo ($page === 'publications' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'publications' ? 'active' : '') ?>">
 		<a href="?page=publications">PUBLICATIONS</a>
 	</li>
-	<li class="<? echo ($page === 'database' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'database' ? 'active' : '') ?>">
 		<a href="?page=database">DATABASE</a>
 	</li>
-	<li class="<? echo ($page === 'contact' ? 'active' : '') ?>">
+	<li class="<?php echo ($page === 'contact' ? 'active' : '') ?>">
 		<a href="?page=contact">CONTACT</a>
 	</li>
 </ul>
