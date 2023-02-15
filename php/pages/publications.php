cc24a59b0bd9f57f94578c693ce6af6451af2168
Micha Hulsbosch <michahulsbosch@gmail.com>
2023-02-15 14:14:55 +0100

    feat: Add new publications

--- php/pages/publications.php
+++ php/pages/publications.php
@@ -6,6 +6,39 @@
 </div>
 <div class="col-xs-12 page-content">
 	<h1 class="visible-xs">Publications</h1>
+	
+	<h2>2023</h2>
+	
+	<p>
+	    R. Jagersma, H. Blom, E. Chayes and A. Hansen (eds.). <i>Private libraries and their documentation, 1665-1830. Studying and interpreting sources</i>, Leiden, Brill, forthcoming in 2023.
+	</p>
+	
+	<p>
+	    R. Jagersma, H. Blom and A. Hansen, ‘Goldmines or Minefields? Private Libraries and Their Documentation (1665-1830)’, in: R. Jagersma, H. Blom, E. Chayes and A. Hansen (eds.). <i>Private libraries and their documentation, 1665-1830. Studying and interpreting sources</i>, Leiden, Brill, forthcoming in 2023.
+	</p>
+	
+	<h2>2022</h2>
+	
+	<p>
+	    A.C. Montoya, <a href="docs/Montoya_Books_of_Hours-2022-.pdf">“Locating books of hours in eighteenth-century private libraries: Auction catalogues as generators of Enlightenment value(s)”</a>, in Malcolm Walsby and Arthur der Weduwen (eds.), <i>The book world of early modern Europe: Essays in honour of Andrew Pettegree</i>, vol. 2, Leiden, Brill. 2022, 433 – 451.
+	</p>
+	
+	<p>
+	    A.C. Montoya, <a href="docs/Montoya_Merian_2022-text-.pdf">“The place of Marie Sibylla Merian’s books in eighteenth-century private libraries”</a>, in Marieke van Delft, Kay Etheridge, Hans Mulder en Florence Pieters (eds.),  <i>Maria Sibylla Merian: Changing the nature of art and science</i>, Tielt – Amsterdam, Lannoo, 2022, 226 – 238.
+	</p>
+	
+	<p>
+	    R. Jagersma, T. Umerle, et al. <i><a href="https://zenodo.org/record/6559857#.Y-zWeBzMIUE">An Analysis of the Current Bibliographical Data Landscape in the Humanities. A Case for the Joint Bibliodata Agendas of Public Stakeholders</a></i>. DARIAH-EU Bibliographical Data Working Group. Zenodo. Open Access, 2022. <a href="https://doi.org/10.5281/zenodo.6559857">https://doi.org/10.5281/zenodo.6559857</a>
+	</p>
+	
+	<h2>2021</h2>
+	
+	<p>
+		H. Blom, “The French connection. Provenance research and the virtual reconstruction of historical libraries à la française”, in Jan Bos, Steven Claeyssens, Erik Geleijns et al. (eds.), <i>Oude boeken, nieuwe podia. Liber amicorum voor Marieke van Delft</i>, Tielt, Lannoo, 2021, 32–38.
+	</p>
+	<p>
+		H. Blom, <a href="docs/cahiers-de-recherches-medievales-et-humanistes-journal-of-medieval-and-humanistic-studies-2021-1-n-41-varia-et-quelques-vieux-livres-1-.pdf">“ « Et quelques vieux livres… » Présence, dénominations et classifications de la « littérature médiévale » dans les catalogues de bibliothèques privées publiés en France (1650-1750)”</a>, <i>Cahiers de recherches médiévales et humanistes - Journal of Medieval and Humanistic Studies</i>, 2021–1, n° 41, 355–375.
+	</p>
 	<p>
 		A.C. Montoya, <a href="https://muse.jhu.edu/article/802447/summary">“Enlightenment? What Enlightenment? Reflections on Half a Million Books (British, French and Dutch Private Libraries, 1665 – 1830)”</a>, <i>Eighteenth-Century Studies</i> 54, 2 (2021),  909 – 934. 
 	</p>
@@ -27,6 +60,9 @@
 	<p>
 		A.C. Montoya, “La dispersion du livre aquitain en Europe: Lecteurs et bibliothèques privées (1665 – 1830)”, <i>Revue française d’histoire du livre</i> 142 (2021), 79 – 104.
 	</p>
+	
+	<h2>2020</h2>
+	
 	<p>
 		A.C. Montoya, <a href="docs/Montoya_2020-Digitizing_Enlightenment.pdf">“Shifting Perspectives and Moving Targets: From Conceptual Perspectives to Bits of Data in the First Year of the MEDIATE project”</a>, in S. Burrows and G. Roe (eds.), <i>Digitizing Enlightenment: Digital Humanities and the Transformation of Eighteenth-Century Studies</i>, Oxford University Studies in the Enlightenment, 2020, 195 – 218.
 	</p>
@@ -40,10 +76,7 @@
 		H. Blom, “Boeken in ballingschap; de catalogus van de bibliotheek van de hugenotenpredikant Isaac Claude (1653-1695)”, <i>Jaarboek voor Nederlandse Boekgeschiedenis 27</i> (2020), 23-43.
 	</p>
 	<p>
-		J. Reboul, <a href="docs/Reboul_2020.pdf">"Counter-Revolutionary Transfers? Emigré Literature and the Subject of the French Emigration in British Private Libraries (1790s - 1830)"</a>, in L. Philip and J. Reboul (eds.), <i>French Emigrants in Revolutionised Europe: Connected Histories and Memories</i>, London, Palgrave Macmillan, 2019, 111 - 134.​
-	</p>
-	<p>
-		H. Blom, R. Jagersma and J. Reboul, ‘Printed Private Library Catalogues as a Source for the History of Reading’, in J. Rose and M. Hammond (eds.), <i>Edinburgh History of Reading</i> (Edinburgh, Edinburgh University Press, 2020), forthcoming.
+		H. Blom, R. Jagersma and J. Reboul, <a href="docs/EHR_Blom_Jagersma_Reboul_Printed_Catalogues_of_Private_Libraries.pdf">‘Printed Private Library Catalogues as a Source for the History of Reading’</a>	, in M. Hammond (ed.), <i>The Edinburgh History of Reading: Early Readers</i>, Edinburgh, Edinburgh University Press, 2020, 249 – 269.
 	</p>
 	<p>
 		R. Jagersma and J. Rozendaal, <a href="docs/15700690-Quaerendo-Female_Book_Ownership_in_the_Eighteenth-Century_Dutch_Republic-1-.pdf">‘Female Book Ownership in the Eighteenth-Century Dutch Republic. The Book Collection of Paper-Cutting Artist Joanna Koerten (1650-1715)’</a>, <i>Quaerendo</i>, 50, 1-2 (2020): 109-140.
@@ -51,15 +84,30 @@
 	<p>
 		H. Blom, <a href="docs/Blom_Helwi.pdf">'Philosophie ou Commerce? L’évolution des systèmes de classement bibliographique dans les catalogues de bibliothèques privées publiés en France au XVIII<sup>e</sup> siècle'</a>, in Frédéric Barbier, István Monok and Andrea Seidler (eds), <i>Les bibliothèques et l’économie des connaissances/Bibliotheken und die Ökonomie des Wissens 1450–1850.</i> Colloque international – Internationale Tagung, 9–13 avril/April 2019 Sárospatak (Hongrie/Ungarn), Budapest, Magyar Tudományos Akadémia Könyvtár és Információs Központ, 2020, pp. 203-234.
 	</p>
+	
+	<h2>2019</h2>
+	
+	<p>
+		J. Reboul, <a href="docs/Reboul_2020.pdf">"Counter-Revolutionary Transfers? Emigré Literature and the Subject of the French Emigration in British Private Libraries (1790s - 1830)"</a>, in L. Philip and J. Reboul (eds.), <i>French Emigrants in Revolutionised Europe: Connected Histories and Memories</i>, London, Palgrave Macmillan, 2019, 111 - 134.​
+	</p>
 	<p>
 		A. E. de Wilde, <a href="docs/Zutot-How_to_Understand_al_yede_Title_Pages_of_Hebrew_Private_Library_Catalogues_Printed_in_the_Dutch_Repu-1-.pdf">'How to Understand <i>ʿal yede</i>? Title Pages of Hebrew Private Library Catalogues Printed in the Dutch Republic during the Long 18th Century'</a>, Zutot 17.1 (2019), 74-82.
 	</p>
+	
+	<h2>2018</h2>
+	
 	<p>
 		A.C. Montoya, <a href="docs/Jaarboek_Nederlandse_boekgeschiedenis.pdf" target="_blank">'The MEDIATE project'</a> <i>Jaarboek voor Nederlandse Boekgeschiedenis / Yearbook for Dutch Book History</i> 25 (2018), 229 - 232.
 	</p>
 	<p>A.C. Montoya and R. Jagersma, <a href="docs/Montoya_and_Jagersma_Book_History_2018.pdf" target="_blank">'Marketing Maria Sibylla Merian, 1720 - 1800. Book Auctions, Gender, and Reading Culture in the Dutch Republic'</a>, <i>Book History</i> 21 (2018), 56 - 88.</p>
+	
+	<h2>2017</h2>
+	
 	<p>A.C. Montoya, <a href="
 http://h-france.net/rude/vol7/montoya7/">'Middlebrow, Religion, and the European Enlightenment: A New Bibliometric Project, <i>MEDIATE</i> (1665 - 1820)'</a>, <em>French History and Civilization</em> 7 (2017), 66 - 79. </p>
+	
+	<h2>Project prehistory</h2>
+	
 	<p>For the project&rsquo;s prehistory, see: A.C. Montoya, <a href="docs/montoya_2004.pdf" target="_blank">&lsquo;French and English Women Writers in Dutch Library Catalogues, 1700-1800.
 	Some Methodological Considerations and Preliminary Results&rsquo;</a>, in S. van Dijk, P. Broomans, J.F. van der Meulen and P. van Oostrum (eds.), 
 	<em>&lsquo;I Have Heard about You&rsquo;. Foreign Women&rsquo;s Writing Crossing the Dutch Border: From Sappho to Selma Lagerl&ouml;f</em>, Hilversum, Verloren, 2004, pp. 182-216.</p>
