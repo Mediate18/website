c1745364c6c7e67c0451071190d46a6972cb2dc3
Micha Hulsbosch <michahulsbosch@gmail.com>
2023-02-15 16:41:51 +0100

    refactor: Changed email addresses to new RU syntax

--- php/pages/contact.php
+++ php/pages/contact.php
@@ -20,7 +20,7 @@
 						<li>The Netherlands</li>
 					</ul>
 				</p>
-				<p>Email: <a href="mailto:a.montoya@let.ru.nl">a.montoya@let.ru.nl</a></p>
+				<p>Email: <a href="mailto:alicia.montoya@ru.nl">a.montoya@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -33,7 +33,7 @@
 				<h2>Prof. dr. Alicia C. Montoya</h2>
 						Alicia Montoya is Principal Investigator of MEDIATE and Professor of French Literature and Culture at Radboud University, The Netherlands. She is the author of <i><a href="http://repository.ubn.ru.nl/handle/2066/105830" target="_blank">Medievalist Enlightenment: From Charles Perrault to Jean-Jacques Rousseau</a></i> (Cambridge 2013), <i>Marie-Anne Barbier et la trag&eacute;die post-classique</i> (Paris 2007, winner of the Keetje Hodson Prize of the Royal Dutch Society for Arts and Sciences) and the co-editor of several volumes, including <i>Early Modern Medievalisms: The Interplay between Scholarly Reflection and Artistic Production</i> (Leiden, 2010), <i>Women Writing Back / Writing Back Women: Transnational Perspectives from the Late Middle Ages to the Dawn of the Modern Era</i> (Leiden, 2010) and <i>Lumi&egrave;res et histoire / Enlightenment and History</i> (Paris 2010). She is an advisory editor for <i>Book History</i> and for Brill's Library of the Written Word series.
 					</p>
-				<p>Email: <a href="mailto:a.montoya@let.ru.nl">a.montoya@let.ru.nl</a></p>
+				<p>Email: <a href="mailto:alicia.montoya@ru.nl">alicia.montoya@ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -47,7 +47,7 @@
 					&eacute;ditions et r&eacute;ceptions de la litt&eacute;rature chevaleresque m&eacute;di&eacute;vale dans la France du dix-septi&egrave;me si&egrave;cle</i> focuses on questions regarding the production, 
 					distribution and consumption of books in early modern France. Helwi is also a participant in the <a href="http://edpop.wp.hum.uu.nl/" target="_blank">EDPOP</a> (The European Dimensions of Popular print) project.
 				</p>
-				<p>Email: <a href="mailto:h.blom@let.ru.nl">h.blom@let.ru.nl</a></p>
+				<p>Email: <a href="mailto:helwi.blom@ru.nl">helwi.blom@ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -61,7 +61,6 @@
 				<p>
 					Evelien Chayes (PhD University of Amsterdam) has authored and co-edited books and published articles on early modern networks in the Veneto, intellectual exchanges in the Venetian ghetto, private libraries and the circulation of (censored) books between France, Italy and Cyprus. At the CNRS Institut de Recherche et d'Histoire des Textes in Paris she worked on the reconstruction of Michel de Montaigne's network in the Parliament of Bordeaux. She is an editorial board member of the <i>Mediterranean Nexus 1100-1700</i> book series, published by Brepols. As a postdoctoral researcher in the ERC project she focuses on Italian private libraries. 
 				</p>
-				<p>Email: <a href="mailto:E.Chayes@let.ru.nl">E.Chayes@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -73,7 +72,6 @@
 				<a name="rindert"></a>			
 				<h2>Dr. Rindert Jagersma</h2>
 				<p>Rindert Jagersma (PhD, University of Amsterdam) is a book historian and cataloguer, specialised in the (quantitative approach of the) book trade of Dutch Republic around 1700. In the MEDIATE project, he focuses on Dutch auction catalogues and their owners. His publications concern book ownership and private book collections in the long eighteenth century; the life and works of the Dutch pamphleteer Ericus Walten (1662-1697) and the importance of pamphleteers and booksellers in the dissemination of the radical Enlightenment; and the identification of the hitherto unknown printers of the works of Benedictus de Spinoza (1632-1677).</p>
-				<p>Email: <a href="mailto:r.jagersma@let.ru.nl">r.jagersma@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -85,7 +83,6 @@
 				<a name="juliette"></a>			
 				<h2>Dr. Juliette Reboul</h2>
 				<p>Juliette Reboul is trained as a historian. She specialises in the circulation of objects, ideas and people in eighteenth-century Europe. She worked on the <a href="http://fbtee.uws.edu.au/main/">FBTEE</a> project (University of Western Sydney), before joining  the MEDIATE team at Radboud University in Nijmegen. In this project, she focuses on printed auction catalogues of British private libraries. Her PhD thesis examined the experiences and memories of French emigrants in the British Isles in response to the French Revolution. It has been published in Palgrave Macmillan's  series  'War, Culture and Society, 1750-1850' as <a href="https://www.palgrave.com/la/book/9783319579955">French Emigration to Great Britain in Response to the French Revolution</a>. </p>
-				<p>Email: <a href="mailto:j.reboul@let.ru.nl">j.reboul@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -97,7 +94,6 @@
 				<a name="anna"></a>			
 				<h2>Anna E. de Wilde, MA</h2>
 				<p>Anna de Wilde participates in the MEDIATE project as a PhD student, focusing on Hebrew and Jewish books in Dutch auction catalogues. She studied History and Jewish Studies at the University of Amsterdam and the Hebrew University of Jerusalem. She specialized in the book culture of the Sephardic community in early modern Amsterdam. Before joining the MEDIATE team Anna worked as a researcher at Ets Haim - Livraria Montezinos, the world's oldest functioning Jewish library.</p>
-				<p>Email: <a href="mailto:A.deWilde@let.ru.nl">A.deWilde@let.ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -111,7 +107,7 @@
 				<a name="micha"></a>			
 				<h2>Micha Hulsbosch, MA</h2>
 				<p>Micha Hulsbosch has a background in Language and Speech Technology. He is part of the <a href="https://www.ru.nl/humanitieslab/technical-support-group/general/">Technical Support Group Humanities Lab</a> and develops software for the Humanities research community, in particular Language research. In the MEDIATE project Micha is mainly tasked with the development of the <a href="?page=database">MEDIATE database</a> webapplication.</p>
-				<p>Email: <a href="mailto:m.hulsbosch@let.ru.nl">M.Hulsbosch@let.ru.nl</a></p>
+				<p>Email: <a href="mailto:micha.hulsbosch@ru.nl">micha.hulsbosch@ru.nl</a></p>
 			</div>
 		</div>
 	</div>
@@ -123,7 +119,7 @@
 				<a name="joanna"></a>			
 				<h2>Joanna Rozendaal, MA</h2>
 				<p>Joanna Rozendaal participates in the MEDIATE project as project assistant. She will be coordinating the transcription project, which includes harvesting, transcribing, and digitizing eighteenth century private library auction catalogues. Joanna holds an MA in Arts and Culture, specialising in Dutch Golden Age Studies, with a particular interest in Book History.</p>
-				<p>Email: <a href="mailto:j.rozendaal@let.ru.nl">j.rozendaal@let.ru.nl</a></p>
+				<p>Email: <a href="mailto:joanna.rozendaal@ru.nl">joanna.rozendaal@ru.nl</a></p>
 			</div>
 		</div>
 	</div>
