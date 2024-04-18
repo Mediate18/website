7e92557aba84262e35df91b25fc0d590bd45c5a6
Micha Hulsbosch <michahulsbosch@gmail.com>
2024-04-18 09:58:36 +0200

    misc: Change text of page 'database'

--- php/pages/database.php
+++ php/pages/database.php
@@ -6,10 +6,11 @@
 </div>
 <div class="col-xs-12 page-content">
 	<h1 class="visible-xs">Database</h1>
-	<br/>
-	<p>We are currently in the process of harvesting, transcribing and enriching data from our corpus of (sales) catalogues of eighteenth-century private libraries. This will form the backbone of two databases: the <strong>BIBLIO database</strong> (Bibliography of Individually-owned Book and Library Inventories Online, 1665 - 1830), containing basic bibliographic records for all extant private library (sales) catalogues in the Dutch Republic, France, and the British Isles, for the period 1665 - 1830; and the <strong>MEDIATE database</strong> (Measuring Enlightenment: Disseminating Ideas, Authors, and Texts in Europe, 1665 - 1830), containing fully searchable transcriptions and metadata on books and collectors extracted from an initial Sandbox corpus of 600 smaller private library (sales) catalogues. It is our aim to incrementally expand this initial Sandbox corpus after the database release in 2022.</p>
-	<p>The public version of the MEDIATE database Sandbox will be launched in the summer of 2022, and will be made available <a href="https://mediate-database.cls.ru.nl">here</a>. 
-	<br/>
+
+	<p>
+		Although the MEDIATE project is formally over, work continues harvesting, transcribing and enriching data from our corpus of (sales) catalogues of eighteenth-century private libraries. The <a href="https://mediate-database.cls.ru.nl/about/">MEDIATE database</a> (Measuring Enlightenment: Disseminating Ideas, Authors, and Texts in Europe, 1665 - 1830), contains fully searchable transcriptions and metadata on books and collectors extracted from a Sandbox corpus of 600 smaller private library (sales) catalogues. It is our aim to incrementally expand this initial Sandbox corpus until 2028. To that end, we have sourced an additional 200 catalogues, including Hebrew-language Dutch catalogues and catalogues from Spain, that we are in the process of turning into searchable data. Work is also ongoing in a second database: the <a href="https://heurist.huma-num.fr/heurist/?db=radboud_BIBLIO&website&id=890&pageid=884">BIBLIO database</a> (Bibliography of Individually-owned Book and Library Inventories Online, 1665 - 1830), containing basic bibliographic records for all extant private library (sales) catalogues in the Dutch Republic, France, and the British Isles, for the period 1665 – 1830.
+	</p>
+
 	<h3>Links to related projects:</h3>
 	<ul>
 		<li><a href="http://primarysources.brillonline.com/browse/book-sales-catalogues-online" target="_blank">Brill Book Sales Catalogues Online </a></li>
